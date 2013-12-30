ad0033cb13c671767c721703676b58c3c3d100bd
William Oliveira <sudowilliam@gmail.com>
2013-12-30 21:06:00 -0200

    adds app token for login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,16 +2,17 @@ class LoginController < ApplicationController
   def create
     cpf_user = CpfUser.find_by e_mail: params[:e_mail]
     cnpj_user = CnpjUser.find_by e_mail: params[:e_mail]
+    login_token = "MYAPPTOKEN"
 
     if cpf_user
       if cpf_user.password == params[:password]
-        render json: cpf_user
+        render json: {user: cpf_user, token: login_token}
       else
         render json: {error: "Invalid password"}
       end
     elsif cnpj_user
       if cnpj_user.password == params[:password]
-        render json: cnpj_user
+        render json: {user: cnpj_user, token: login_token}
       else
         render json: {error: "Invalid password"}
       end
