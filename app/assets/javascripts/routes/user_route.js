1876b68b49de6410037bfb8556dd201a21b46e69
William Oliveira <sudowilliam@gmail.com>
2013-12-30 20:00:22 -0200

    removes unused action in route

--- app/assets/javascripts/routes/user_route.js
+++ app/assets/javascripts/routes/user_route.js
@@ -1,9 +1,5 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EmberCommerce.UserRoute = Ember.Route.extend({
-  actions: {
-    login: function () {
-      console.log('it works!');
-    }
-  }
+
 });
