71aa51d509db3347105db22c0b65f9e2ec86fe3a
William Oliveira <sudowilliam@gmail.com>
2013-12-30 20:12:43 -0200

    changes method name to implement post

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,5 @@
 class LoginController < ApplicationController
-  def index
+  def create
     cpf_user = CpfUser.find_by e_mail: params[:e_mail]
     cnpj_user = CnpjUser.find_by e_mail: params[:e_mail]
 
