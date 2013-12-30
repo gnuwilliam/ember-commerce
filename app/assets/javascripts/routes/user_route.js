bc4432e8f712b4d779ec1d38b835693ccb285441
William Oliveira <sudowilliam@gmail.com>
2013-12-30 15:27:32 -0200

    triggering action

--- app/assets/javascripts/routes/user_route.js
+++ app/assets/javascripts/routes/user_route.js
@@ -1,5 +1,9 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EmberCommerce.UserRoute = Ember.Route.extend({
-  
+  actions: {
+    login: function () {
+      console.log('it works!');
+    }
+  }
 });
