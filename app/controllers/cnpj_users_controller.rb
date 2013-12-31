327429086598c7fad08729cdc2e05fb77c74d341
William Oliveira <sudowilliam@gmail.com>
2013-12-31 02:54:50 -0200

    fix cnpj_user controller

--- app/controllers/cnpj_users_controller.rb
+++ app/controllers/cnpj_users_controller.rb
@@ -1,11 +1,21 @@
 class CnpjUsersController < ApplicationController
 	def create
 		sleep 1
-		return json: CnpjUser.create(cnpj_user)
+		render json: CnpjUser.create(cnpj_user)
 	end
 
 	private
 	def cnpj_user
-		params[:cnpj_user].permit(:cnpj, :fantasy_name, :state_registration, :full_name, :phone, :e_mail, :address, :transporter, :password
+		params[:cnpj_user].permit(
+      :cnpj, 
+      :fantasy_name, 
+      :state_registration, 
+      :full_name, 
+      :phone, 
+      :e_mail, 
+      :address, 
+      :transporter, 
+      :password
+    )
 	end
 end
