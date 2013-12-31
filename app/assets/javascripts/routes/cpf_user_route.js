3e3df3923fdb7f5a33fa6b5f871bee06ca013a47
William Oliveira <sudowilliam@gmail.com>
2013-12-31 18:01:50 -0200

    removing create method from route

--- app/assets/javascripts/routes/cpf_user_route.js
+++ app/assets/javascripts/routes/cpf_user_route.js
@@ -3,15 +3,5 @@
 EmberCommerce.CpfUserRoute = Ember.Route.extend({
   model: function () {
     return this.store.createRecord('cpf_user');
-  },
-
-  actions: {
-    create: function (cpf_user) {
-      var route = this;
-
-      cpf_user.save().then(function () {
-        route.transitionTo('user');
-      });
-    }
   }
 });
