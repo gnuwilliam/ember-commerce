4a86e3392b621f1d68c6bf6dfb7ff7f33af595f5
William Oliveira <sudowilliam@gmail.com>
2013-12-31 02:55:18 -0200

    implements login and logout - save the user data

--- app/assets/javascripts/controllers/user.js
+++ app/assets/javascripts/controllers/user.js
@@ -1,15 +1,48 @@
 EmberCommerce.UserController = Ember.Controller.extend({
+  reset: function() {
+    this.setProperties({
+      e_mail: "",
+      password: ""
+    });
+  },
+
+  token: localStorage.token,
+  loggedIn: localStorage.loggedIn,
+  userData: localStorage.userData,
+
+  tokenChanged: function() {
+    localStorage.token = this.get('token');
+  }.observes('token'),
+
+  loggedInChanged: function() {
+    localStorage.loggedIn = this.get('loggedIn');
+  }.observes('loggedIn'),
+
+  userDataChanged: function() {
+    localStorage.userData = this.get('userData');
+  }.observes('userData'),
+
   actions: {
     login: function() {
+      var self = this;
       var user_data = this.getProperties('e_mail', 'password');
 
       Ember.$.post('/login', user_data).then(function (response) {
         if (response.error) {
           return alert(response.error);
         }
-        console.log(response.token);
-        console.log(response.user);
+
+        self.set('token', response.token);
+        self.set('loggedIn', true);
+        self.set('userData', response.user);
       });
+    },
+
+    logout: function() {
+      this.set('token', false);
+      this.set('loggedIn', false);
+      this.set('userData', false);
+      this.transitionToRoute('main');
     }
   }
 });
