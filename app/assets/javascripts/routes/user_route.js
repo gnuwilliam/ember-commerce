5a66db76ec9502a7a090de501351666702d514ee
William Oliveira <sudowilliam@gmail.com>
2013-12-31 02:54:33 -0200

    resets the controller when routes are changed

--- app/assets/javascripts/routes/user_route.js
+++ app/assets/javascripts/routes/user_route.js
@@ -1,5 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EmberCommerce.UserRoute = Ember.Route.extend({
-
+  setupController: function(controller, context) {
+    controller.reset();
+  }
 });
